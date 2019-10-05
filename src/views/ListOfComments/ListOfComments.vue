76ae0ded03bf0a8de06f61505ed3701c329539eb
hassnian <hassnianxd@gmail.com>
2019-10-05 20:59:01 +0200

    fect

---
+++ src/views/ListOfComments/ListOfComments.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <b-spinner v-if="loading" variant="success" label="Spinning"></b-spinner>
+    <b-list-group v-else-if="data" v-for="comment in data">
+      <b-list-group-item v-if="comment.body !== '[deleted]'" button
+        ><b>{{ comment.author }}</b> {{ comment.body }}</b-list-group-item
+      >
+    </b-list-group>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+import data from "@/assets/data.js";
+
+@Component
+export default class SelectNetwork extends Vue {
+  data = data;
+  loading = true;
+
+  created() {
+    setTimeout(() => {
+      this.loading = false;
+    }, 3000);
+    data.forEach((comment: any) => console.log(comment));
+  }
+}
+</script>

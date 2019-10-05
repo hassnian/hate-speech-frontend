fdf645e55452723f510b8c0611d28d70c87fd577
noemisg <nsoriaga7@gmail.com>
2019-10-05 19:35:39 +0200

    form OK

--- src/components/SelectNetwork.vue
+++ src/components/SelectNetwork.vue
@@ -1,11 +1,37 @@
 <template>
   <div class="container d-flex h-100">
-    <div class="row ustify-content-center align-self-center">
-      
-
-     
+    <div class="row justify-content-center">
+      <b-container>
+        <h3>Selecciona una red social</h3>
+      </b-container>
+      <b-container class="mt-5">
+        <b-row>
+          <b-col cols="4">
+            <b-img :src="require('@/assets/facebook.png')" fluid alt="Responsive image" width="60"></b-img>
+          </b-col>
+          <b-col cols="4">
+            <b-img :src="require('@/assets/reddit.png')" fluid alt="Responsive image" width="60"></b-img>
+          </b-col>
+          <b-col cols="4">
+            <b-img :src="require('@/assets/twitter.png')" fluid alt="Responsive image" width="60"></b-img>
+          </b-col>
+        </b-row>
+      </b-container>
+      <b-container class="mt-5">
+        <h4>o</h4>
+      </b-container>
+      <b-container>
+        <h3>Introduce un texto</h3>
+        <div class="mt-5">
+          <b-form-textarea id="textarea" v-model="text" placeholder="Introduce un texto..." rows="3" max-rows="6"></b-form-textarea>
+          <b-form-checkbox id="checkbox-1" v-model="status" name="checkbox-1" value="accepted" unchecked-value="not_accepted" class="mt-4">Acepto los términos y condiciones de uso</b-form-checkbox>
+          <div class="sr-only">State: <strong>{{ status }}</strong></div>
+          <div class="mt-5 mb-5">
+            <b-button pill>Consultar</b-button>
+          </div>
+        </div>
+      </b-container>
     </div>
-    
   </div>
 </template>
 
@@ -14,8 +40,14 @@ import { Component, Prop, Vue } from 'vue-property-decorator';
 
 @Component
 export default class SelectNetwork extends Vue {
+  data() {
+    return {
+      status: 'not_accepted'
+    }
+  }
   
 }
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
